80ddd62637b86832d0de03e6e02e3d1c1fe97265
a.yaacoud <a.yaacoub@ydigitalmedia.com>
2020-02-17 15:49:57 +0000

    Fixed bugs, added items on form

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $('textarea').on('keydown', function(){
         $(this).css('background-color', 'white');
+        $(this).css("background", "url(Pictures/icon-check.png) left top no-repeat")
     });
 });
 
